b6e06e482caa2712324d3f38e2d3bea4eb577bb7
Deivid Alves <deividalvescb@gmail.com>
2020-12-05 10:31:43 -0300

    Primeiro commit

---
+++ js/index.js
@@ -0,0 +1,40 @@
+var nome = "Deivid A.";
+var cargo = "Programador";
+
+var nomehtml = document.getElementById("nome-no-html");
+var cargohtml = document.getElementById("cargo-no-html");
+var texto1 = document.getElementById("texto-1");
+var texto2 = document.getElementById("texto-2");
+
+
+function logarNome(){
+    console.log(nome);
+}
+
+
+function colocarNomeNoHtml(nome){
+    nomehtml.innerHTML = nome;
+
+}
+
+
+
+
+function colocarCargoNoHtml(cargo){
+    cargohtml.innerHTML = cargo;
+}
+
+function clickNoProjetos(){
+    console.log("Clicou no botão projetos");
+    texto2.style.display= "block";
+    texto1.style.display= "none";
+}
+
+function clickNoSobre(){
+    console.log("clicou no botão sobre");
+    texto1.style.display = "block";
+    texto2.style.display = "none";
+}
+
+colocarNomeNoHtml(nome);
+colocarCargoNoHtml(cargo);
